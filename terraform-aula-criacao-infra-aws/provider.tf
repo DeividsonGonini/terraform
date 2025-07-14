8729da37f3200e5dabbf76cd09e3760f1f4f5832
Deividson Gonini <deividsongonini@gmail.com>
2025-07-14 00:24:43 -0300

    manifest e configs do eks

--- terraform-aula-criacao-infra-aws/provider.tf
+++ terraform-aula-criacao-infra-aws/provider.tf
@@ -1,3 +1,34 @@
 provider "aws" {
   region = var.region_default
 }
+
+# Aula 8 p.03 
+# Kubectl_Manifest
+#https://registry.terraform.io/providers/gavinbunney/kubectl/latest/docs
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.19.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.36.0"
+    }
+  }
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.auth.token
+  load_config_file       = false
+}
+
+#Provider Kubernets
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.auth.token
+}
+#######
