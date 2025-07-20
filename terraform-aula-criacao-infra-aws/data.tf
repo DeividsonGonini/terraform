f5e764717296c63d256a0407b18bffdcc71adc94
Deividson Gonini <deividsongonini@gmail.com>
2025-07-20 15:05:39 -0300

    tirando criacao do kubernet

--- terraform-aula-criacao-infra-aws/data.tf
+++ terraform-aula-criacao-infra-aws/data.tf
@@ -5,12 +5,12 @@ data "aws_iam_user" "principal_user" {
 
 #Aula 8 p.03
 #Valores utilizados pelo provicer kubectl (do Manifest) 
-data "aws_eks_cluster" "cluster" {
-  name = aws_eks_cluster.cluster.name
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = aws_eks_cluster.cluster.name
+# }
 
-data "aws_eks_cluster_auth" "auth" {
-  name = aws_eks_cluster.cluster.name
-}
+# data "aws_eks_cluster_auth" "auth" {
+#   name = aws_eks_cluster.cluster.name
+# }
 
 ###
