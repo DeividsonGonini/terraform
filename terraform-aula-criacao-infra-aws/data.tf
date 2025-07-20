9a8d250f27db4e69cdc2a3644656208dab24b260
Deividson Gonini <51547739+DeividsonGonini@users.noreply.github.com>
2025-07-20 13:11:26 -0300

    Update data.tf

--- terraform-aula-criacao-infra-aws/data.tf
+++ terraform-aula-criacao-infra-aws/data.tf
@@ -1,7 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_user
 data "aws_iam_user" "principal_user" {
   user_name = "11soat-tf" # Identificador do usuario que queremos as infos. Para contas que não são AWS Academy
-  arn = "arn:aws:sts::018779781627:assumed-role/voclabs/user4172085=deividsongonini@gmail.com"
+  user_id = "AROAQIX24XH55PW72YKRB:user4172085=deividsongonini@gmail.com"
 }
 
 #Aula 8 p.03
