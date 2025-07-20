ddcb8e8101a3ce7c36d68767c145c558ef496c6d
Deividson Gonini <deividsongonini@gmail.com>
2025-07-20 14:55:28 -0300

    comentando providers do manifest

--- terraform-aula-criacao-infra-aws/provider.tf
+++ terraform-aula-criacao-infra-aws/provider.tf
@@ -18,17 +18,17 @@ terraform {
   }
 }
 
-provider "kubectl" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.auth.token
-  load_config_file       = false
-}
+# provider "kubectl" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.auth.token
+#   load_config_file       = false
+# }
 
-#Provider Kubernets
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.auth.token
-}
+# #Provider Kubernets
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.auth.token
+# }
 #######
